5f6db1195385354eb4130e43234503ab5afde1b8
crunky89 <crunky8979@gmail.com>
2022-03-04 17:43:03 +0900

    리액트 환경설정 및 깃 배포

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +23,14 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">To Do List</h4>
+      </div>
+    )
+  }
+}
